44869a6212342a3b28f3a4ba8f2aca8219d97867
jfsummersdtcc <jfsummersdtcc@gmail.com>
2017-03-09 19:28:12 -0500

    marg is no better.

--- MyRepo/src/com/repo/MyRepoRunner.java
+++ MyRepo/src/com/repo/MyRepoRunner.java
@@ -2,7 +2,7 @@ package com.repo;
 
 public class MyRepoRunner {
 
-	public static void main(String[] margs) {
+	public static void main(String[] marg) {
 		
 		System.out.println("This is my repo, man!!!!");;;;
 	}
