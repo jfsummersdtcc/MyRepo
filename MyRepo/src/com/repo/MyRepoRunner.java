65210cabdf6c3b4f07421a12e15c308bae45af43
jfsummersdtcc <jfsummersdtcc@gmail.com>
2017-03-09 22:17:15 -0500

    ok

--- MyRepo/src/com/repo/MyRepoRunner.java
+++ MyRepo/src/com/repo/MyRepoRunner.java
@@ -2,7 +2,7 @@ package com.repo;
 
 public class MyRepoRunner {
 
-	public static void main(String[] marg) {
+	public static void main(String[] arg) {
 		
 		System.out.println("This is my repo, man!!!!");;;;
 	}
