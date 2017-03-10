7033a614b5278804b1bdb64d7b2ecd978aebf947
jfsummersdtcc <jfsummersdtcc@gmail.com>
2017-03-09 19:15:13 -0500

    what is a marg?

--- MyRepo/src/com/repo/MyRepoRunner.java
+++ MyRepo/src/com/repo/MyRepoRunner.java
@@ -2,7 +2,7 @@ package com.repo;
 
 public class MyRepoRunner {
 
-	public static void main(String[] args) {
+	public static void main(String[] margs) {
 		
 		System.out.println("This is my repo, man!!!!");;;;
 	}
