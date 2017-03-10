f99ba734215c536a05f62fa91268c46652fe626f
jfsummersdtcc <jfsummersdtcc@gmail.com>
2017-03-09 22:20:05 -0500

    commit

--- MyRepo/src/com/repo/MyRepoRunner.java
+++ MyRepo/src/com/repo/MyRepoRunner.java
@@ -2,7 +2,7 @@ package com.repo;
 
 public class MyRepoRunner {
 
-	public static void main(String[] arg) {
+	public static void main(String[] args) {
 		
 		System.out.println("This is my repo, man!!!!");;;;
 	}
